5851cdf73a7fd7f635edf81046a5d659adfb1645
Andrine Kirstine Gudbrandsen <andrine.kirstine@gmail.com>
2023-10-28 15:50:03 +0200

    Update userController.ts

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,12 +1,24 @@
+import { StringExpression } from "mongoose";
 import User, { IUser, UserDoc } from "../models/userModel";
 
-export const addUsers = async(userId: string, labels: boolean, activityIds: string[]) => {
+const labelList: string[] = [ "010", "020", "021", "052", "053", "056", "058", "059", "060", "062",
+    "064", "065", "067", "068", "069", "073", "075", "076", "078", "080",
+    "081", "082", "084", "085", "086", "087", "088", "089", "091", "092",
+    "096", "097", "098", "100", "101", "102", "104", "105", "106", "107",
+    "108", "110", "111", "112", "114", "115", "116", "117", "118", "124",
+    "125", "126", "128", "129", "136", "138", "139", "141", "144", "147",
+    "153", "154", "161", "163", "167", "170", "174", "175", "179"];
+
+
+export const addUsers = async(userId: string, activityIds: string[]) => {
     if (userId === undefined || !userId) {
         return console.log("Need userId")
     }
     if (activityIds === undefined || activityIds.length <= 0) {
         return console.error("Need activity ids")
     }
+
+    const labels: boolean = labelList.includes(userId)
 
     const newUser: IUser = {
         _id: userId,
